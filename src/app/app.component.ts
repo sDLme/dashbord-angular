3b0ab37bea13ac233a44108893d2e791ee49d7bf
sDLme <marina2606@gmail.com>
2020-04-02 19:28:03 +0300

    add info

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
@@ -8,15 +8,30 @@ import { HttpErrorResponse } from '@angular/common/http';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit{
-  title = 'Temperature chart info';
-  tempObj: any;
-  constructor(private httpService: HttpClient) {
+  title = 'Temperature info';
+  tempObj: [];
+  powerObj = {voltage: 0, current: 0, power: 0, energy: 0, frequency: 0};
+  constructor(private httpService: HttpClient, private query: HttpClient ) {
   }
   ngOnInit() {
+    this.loadData();
+    setInterval( () =>  {
+      this.loadData();
+    }, 3000);
+  }
+  loadData() {
     this.httpService.get('http://192.168.1.5/temperature.php').subscribe(
       response => {
         // @ts-ignore
         this.tempObj = response.data;
+      },
+      (err: HttpErrorResponse) => {
+        console.log (err.message);
+      });
+    this.query.get('http://192.168.1.133/info.json').subscribe(
+      response => {
+        // @ts-ignore
+        this.powerObj = response;
       },
       (err: HttpErrorResponse) => {
         console.log (err.message);
--- src/app/chart/chart.component.ts
+++ src/app/chart/chart.component.ts
@@ -1,4 +1,5 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, Input, OnChanges, OnInit} from '@angular/core';
+import {range} from 'rxjs';
 
 
 @Component({
@@ -6,19 +7,45 @@ import {Component, Input, OnInit} from '@angular/core';
   templateUrl: './chart.component.html',
   styleUrls: ['./chart.component.scss']
 })
-export class ChartComponent {
+export class ChartComponent implements OnInit, OnChanges {
 
   @Input() tempValue: number;
   @Input() tempLabel: string;
   @Input() temp: string;
-  canvasWidth = 500;
+  @Input() min = 0;
+  @Input() max  = 100;
+  @Input() step  = 20;
+  canvasWidth = 300;
   options = {
     arcLabels: [ '20', '40', '60', '80'],
     hasNeedle: true,
-    needleUpdateSpeed: 1000,
+    needleUpdateSpeed: 0,
     needleStartValue: 50,
     arcColors: ['rgb(104, 237, 43)', 'rgb(195, 237, 43)', 'rgb(237, 205, 43)', 'rgb(237, 159, 43)', 'rgb(237, 62, 43)'],
     arcDelimiters: [20, 40, 60, 80],
-    rangeLabel: ['0', '100'],
+    rangeLabel: [ '0', '100'],
   };
+   convertRange( value: number, r1, r2 ): number {
+     return ( value - r1[ 0 ] ) * ( r2[ 1 ] - r2[ 0 ] ) / ( r1[ 1 ] - r1[ 0 ] ) + r2[ 0 ];
+  }
+  myRange(min: number, max: number, step: number) {
+     const arr = [];
+     let count = min;
+     let stop = 0;
+     while ( (count < max) && stop < 100) {
+       stop++;
+       count  = count + step;
+       arr.push(count);
+    }
+     return arr;
+  }
+  ngOnInit() {
+      this.options.rangeLabel = [ this.min + '', this.max + ''];
+      const newStep = (this.max - this.min) / 5;
+      this.options.arcLabels = this.myRange(this.min, this.max, newStep);
+      this.tempValue = this.convertRange(this.tempValue, [this.min , this.max ], [ 0, 100] );
+  }
+  ngOnChanges() {
+    this.tempValue = this.convertRange(this.tempValue, [this.min , this.max ], [ 0, 100] );
+  }
 }
